e92e17e3b07480a65ec70af0c6622ef2d9cb19bd
Thibaut Moreau <thibaut.moreau.massart@gmail.com>
2020-02-18 17:06:38 +0100

    Models & CRUD - Rails Task Manager step6

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,9 +1,35 @@
 class TasksController < ApplicationController
-  def index
+  def index #GET / tasks
     @tasks = Task.all
   end
 
-  def show
+  def show #GET / tasks/:id
     @task = Task.find(params[:id])
   end
+
+  def new #GET / tasks/new
+    @task = Task.new
+  end
+
+  def create #POST / tasks
+    @task = Task.new(task_params)
+    @task.save
+    redirect_to tasks_path
+  end
+
+  def edit
+    @task = Task.find(params[:id])
+  end
+
+  def update
+    @task = Task.find(params[:id])
+    @task.update(task_params)
+    redirect_to tasks_path
+  end
+
+  private
+  def task_params
+    params.require(:task).permit(:title, :details, :completed)
+  end
+
 end
